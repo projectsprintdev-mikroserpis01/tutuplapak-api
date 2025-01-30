0772d1c5b87ec3b8622c621b6ac5b7bba35e38b9
afrizaloky <afrizaloky@outlook.com>
2025-01-30 08:12:34 +0700

    remove proof

--- database/migrations/20250124042309_create_purchase_table.up.sql
+++ database/migrations/20250124042309_create_purchase_table.up.sql
@@ -4,7 +4,6 @@ CREATE TABLE purchase (
     sender_name VARCHAR(255) NOT NULL,
     sender_contact_type VARCHAR(50) NOT NULL,
     sender_contact_detail VARCHAR(255) NOT NULL,
-    payment_proof_ids VARCHAR(255)[],
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
